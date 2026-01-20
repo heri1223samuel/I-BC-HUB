eb46205cdd9f71d60773b41774d6ba7dffad9d0a
Heritiana Hasina <herysamuelpljv@gmail.com>
2026-01-20 17:29:51 +0300

    feat(media) import image on the article

--- src/custom/icon.ts
+++ src/custom/icon.ts
@@ -1,9 +1,21 @@
+import culture from "../assets/image/culture.jpg";
+import cyc from "../assets/image/Cyclones.jpeg";
+import econom from "../assets/image/economie.webp";
+import education from "../assets/image/education.avif";
+import nigeria from "../assets/image/nigeria.webp";
+import tourisme from "../assets/image/tourisme.jpg";
 import user from "../assets/user.avif";
 import logo from "../logo.png";
 
 const sharedMedia = {
   user,
   logo,
+  econom,
+  cyc,
+  nigeria,
+  tourisme,
+  education,
+  culture,
 };
 const darkMedia = {};
 
