ca7f0d8aa945221f8f33e1bb1538cbbf38f60239
Heritiana Hasina <herysamuelpljv@gmail.com>
2026-01-20 17:30:15 +0300

    Create constant of article category

---
+++ src/data/newsCategory.ts
@@ -0,0 +1,83 @@
+export const NEWS_CATEGORY = {
+  "Actualités générales": [
+    "Politique",
+    "Économie",
+    "Société",
+    "International",
+    "Sécurité",
+    "Justice",
+    "Diplomatie",
+  ],
+
+  "Environnement & urgences": [
+    "Météo",
+    "Catastrophes naturelles",
+    "Environnement",
+    "Climat",
+    "Santé publique",
+    "Urgent",
+  ],
+
+  Sport: [
+    "Football",
+    "Basketball",
+    "Autres sports",
+    "Compétitions internationales",
+    "Sport local",
+  ],
+
+  "Culture & loisirs": [
+    "Culture",
+    "Musique",
+    "Cinéma",
+    "Arts",
+    "Littérature",
+    "Festivals",
+    "Patrimoine",
+  ],
+
+  "Éducation & emploi": [
+    "Éducation",
+    "Universités",
+    "Formation",
+    "Emploi",
+    "Jeunesse",
+  ],
+
+  "Technologie & innovation": [
+    "Technologie",
+    "Numérique",
+    "Innovation",
+    "Startups",
+    "Télécommunications",
+  ],
+
+  Santé: ["Santé", "Médecine", "Prévention", "Bien-être"],
+
+  "Économie locale & développement": [
+    "Agriculture",
+    "Élevage",
+    "Pêche",
+    "Industrie",
+    "Infrastructures",
+    "Transport",
+  ],
+
+  "Vie citoyenne": [
+    "Communautés locales",
+    "Vie associative",
+    "ONG",
+    "Droits humains",
+    "Égalité",
+    "Inclusion sociale",
+  ],
+
+  "Insolite & divertissement": ["Insolite", "Faits divers", "Buzz", "People"],
+
+  "Spécifique Madagascar": [
+    "Régions",
+    "Collectivités locales",
+    "Développement rural",
+    "Culture traditionnelle",
+  ],
+} as const;
