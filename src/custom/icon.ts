bd4b8cfd0209b93ecc69242a8025581519c90a27
Heritiana Hasina <herysamuelpljv@gmail.com>
2026-01-22 10:25:52 +0300

    Import image for emission

--- src/custom/icon.ts
+++ src/custom/icon.ts
@@ -2,13 +2,21 @@ import culture from "../assets/image/culture.jpg";
 import cyc from "../assets/image/Cyclones.jpeg";
 import econom from "../assets/image/economie.webp";
 import education from "../assets/image/education.avif";
+import i_vazo from "../assets/image/i_vazo.jpg";
+import tapotandroka from "../assets/image/kapotandroka.jpg";
+import menaMasoandro from "../assets/image/mena.jpg";
 import nigeria from "../assets/image/nigeria.webp";
 import tourisme from "../assets/image/tourisme.jpg";
+import tsara_kaly from "../assets/image/trarakaly.jpg";
 import user from "../assets/user.avif";
 import logo from "../logo.png";
 
 const sharedMedia = {
   user,
+  menaMasoandro,
+  i_vazo,
+  tsara_kaly,
+  kapotandroka: tapotandroka,
   logo,
   econom,
   cyc,
