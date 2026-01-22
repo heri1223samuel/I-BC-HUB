8fd7aef85201c152ab153aae81ad4b92bc5d857f
Heritiana Hasina <herysamuelpljv@gmail.com>
2026-01-22 10:48:13 +0300

    Import manga and grain de pollen emission image

--- src/custom/icon.ts
+++ src/custom/icon.ts
@@ -2,9 +2,11 @@ import culture from "../assets/image/culture.jpg";
 import cyc from "../assets/image/Cyclones.jpeg";
 import econom from "../assets/image/economie.webp";
 import education from "../assets/image/education.avif";
+import grain_de_polen from "../assets/image/GDP.jpg";
 import i_vazo from "../assets/image/i_vazo.jpg";
 import tapotandroka from "../assets/image/kapotandroka.jpg";
 import menaMasoandro from "../assets/image/mena.jpg";
+import manga from "../assets/image/MG.jpg";
 import nigeria from "../assets/image/nigeria.webp";
 import tourisme from "../assets/image/tourisme.jpg";
 import tsara_kaly from "../assets/image/trarakaly.jpg";
@@ -21,6 +23,8 @@ const sharedMedia = {
   econom,
   cyc,
   nigeria,
+  manga,
+  grain_de_polen,
   tourisme,
   education,
   culture,
